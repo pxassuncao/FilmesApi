b45e4f415bed8e1946b56494de1b358d322f2476
pxassuncao <mirassuncaobjj07@gmail.com>
2024-03-06 18:30:14 -0300

    melhorias controller

---
+++ FilmesApi/Controllers/CinemaController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CinemaController : ControllerBase
+    {
+        private FilmeContext _context;
+        private IMapper _mapper;
+
+        public CinemaController (FilmeContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaCinema([FromBody] CreateCinemaDto cinemaDto)
+        {
+            Cinema cinema = _mapper.Map<Cinema>(cinemaDto);
+            _context.Cinemas.Add(cinema);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaCinemasPorId)), new {Id = cinema.Id}, cinemaDto;
+        }
+    }
+}
--- FilmesApi/Data/Dtos/ReadCinemaDto.cs
+++ FilmesApi/Data/Dtos/ReadCinemaDto.cs
@@ -1,6 +1,12 @@
-﻿namespace FilmesApi.Data.Dtos
+﻿
+
+namespace FilmesApi.Data.Dtos
 {
     public class ReadCinemaDto
     {
+       
+        public int id { get; set; }
+       
+        public string Nome { get; set; }
     }
 }
